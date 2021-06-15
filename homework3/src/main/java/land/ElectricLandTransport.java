c1bbcebea9f246108de1cfee54138f42705f5987
Anna Lialikova <anna-hess@mail.ru>
2021-06-15 11:54:29 +0300

    homework3 done

---
+++ homework3/src/main/java/land/ElectricLandTransport.java
@@ -0,0 +1,11 @@
+public class ElectricLandTransport {
+    private int electricityConsumption;
+
+    public int getElectricityConsumption() {
+        return electricityConsumption;
+    }
+
+    public void setElectricityConsumption(int electricityConsumption) {
+        this.electricityConsumption = electricityConsumption;
+    }
+}
---
+++ homework3/src/main/java/land/LandTransport.java
@@ -0,0 +1,11 @@
+public class LandTransport {
+    private String route;
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+}
